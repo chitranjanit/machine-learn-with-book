ab92331aef733447db74d1aa62ab546b8f70cbc9
chitranjan <chitranjan.it@gmail.com>
2019-12-28 17:10:02 +0545

    multiline statement

---
+++ basics/multiline-statement.py
@@ -0,0 +1,11 @@
+# Example of implicit line continuation
+x = '1' + '2' + '3' + '4'
+# Example of explicit line continuation
+y = '1' + '2' + '11' + '12'
+weekdays = ['Monday', 'Tuesday', 'Wednesday','Thursday', 'Friday']
+weekend = {'Saturday','Sunday'}
+print('x has a value of', x)
+print('y has a value of', y)
+print( weekend )
+
+#Excerpt From: Manohar Swamynathan. “Mastering Machine Learning with Python in Six Steps.” Apple Books.

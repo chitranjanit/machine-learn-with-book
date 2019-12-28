aaa69ca14d9193b034a8659e185428c7957b3923
chitranjan <chitranjan.it@gmail.com>
2019-12-28 17:00:00 +0545

    learn python

---
+++ basics/comment.py
@@ -0,0 +1,6 @@
+# This is a single line comment in Python
+
+""" This is an example of a multi line
+comment that runs into multiple lines.
+Everything that is in between is considered as comments
+"""
